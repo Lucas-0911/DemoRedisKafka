da4fcd7794c5b72a7189135836221a84cf2cde78
Lucas <phuclq0911@gmail.com>
2025-06-05 15:28:34 +0700

    Kafka cloud

--- Common/src/main/java/com/lucas/common/redis/RedisConnection.java
+++ Common/src/main/java/com/lucas/common/redis/RedisConnection.java
@@ -3,8 +3,8 @@ package com.lucas.common.redis;
 import redis.clients.jedis.Jedis;
 
 public class RedisConnection {
-
-    private static final String HOST = "127.0.0.1";
+//private static final String HOST = "127.0.0.1";
+    private static final String HOST = "14.225.36.39";
     private static final int PORT = 6319;
     private static final String PASSWORD = "";
 
